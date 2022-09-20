e5fb6620a257ae0d1b651868df74a3ab969a03c3
GitHub Action <actions@github.com>
2022-09-20 20:33:35 +0000

    [CI] build hugo static site

---
+++ js/common.js
@@ -0,0 +1,15 @@
+(() => {
+  // <stdin>
+  document.addEventListener("DOMContentLoaded", function() {
+    const intersectionObserver = new IntersectionObserver(function(entries) {
+      const topBtn = document.querySelector(".top-of-site-link");
+      if (topBtn === null)
+        return;
+      topBtn.dataset.visible = entries[0].boundingClientRect.y < 0;
+    });
+    const topAnchor = document.querySelector("#top-of-site-anchor");
+    if (topAnchor !== null) {
+      intersectionObserver.observe(topAnchor);
+    }
+  });
+})();
